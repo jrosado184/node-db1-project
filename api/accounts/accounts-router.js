4faa03170f56f59c2946ee3787bb931ced09b014
jrosado184 <88807540+jrosado184@users.noreply.github.com>
2022-01-17 12:44:57 -0500

    built get enpoint and database function

--- api/accounts/accounts-router.js
+++ api/accounts/accounts-router.js
@@ -1,27 +1,37 @@
-const router = require('express').Router()
+const router = require("express").Router();
+const Account = require("./accounts-model");
 
-router.get('/', (req, res, next) => {
-  // DO YOUR MAGIC
-})
+router.get("/", (req, res, next) => {
+  Account.getAll()
+    .then((data) => {
+      res.json(data);
+    })
+    .catch(next);
+});
 
-router.get('/:id', (req, res, next) => {
-  // DO YOUR MAGIC
-})
-
-router.post('/', (req, res, next) => {
-  // DO YOUR MAGIC
-})
-
-router.put('/:id', (req, res, next) => {
+router.get("/:id", (req, res, next) => {
   // DO YOUR MAGIC
 });
 
-router.delete('/:id', (req, res, next) => {
+router.post("/", (req, res, next) => {
   // DO YOUR MAGIC
-})
+});
 
-router.use((err, req, res, next) => { // eslint-disable-line
+router.put("/:id", (req, res, next) => {
   // DO YOUR MAGIC
-})
+});
+
+router.delete("/:id", (req, res, next) => {
+  // DO YOUR MAGIC
+});
+
+router.use((err, req, res, next) => {
+  // eslint-disable-line
+  res.status(500).json({
+    message: err.message,
+    custom: "oops, an error occurred in the accounts router ",
+    status: err.status,
+  });
+});
 
 module.exports = router;
